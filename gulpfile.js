996f54d40de91c626e4746096f69545cc49a8faa
Héctor Aristimuño <soullade@gmail.com>
2014-08-27 21:13:48 -0430

    Commit inicial

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+"use strict"
+
+// -- DEPENDENCIES -------------------------------------------------------------
+var gulp        = require('gulp');
+var html        = require('gulp-markdown');
+var pdf         = require('gulp-markdown-pdf');
+var connect     = require('gulp-connect');
+
+// -- FILES --------------------------------------------------------------------
+var path = {
+  build : 'build'};
+
+var source = {
+  md: '*.md'};
+
+// -- TASKS --------------------------------------------------------------------
+gulp.task('html', function () {
+    return gulp.src(source.md)
+        .pipe(html())
+        .pipe(gulp.dest(path.build))
+        .pipe(connect.reload());
+});
+
+gulp.task('pdf', function () {
+    return gulp.src(source.md)
+        .pipe(pdf())
+        .pipe(gulp.dest(path.build))
+        .pipe(connect.reload());
+});
+
+gulp.task('webserver', function() {
+  connect.server({ port: 8000, root: 'build/', livereload: true });
+});
+
+gulp.task('init', function() {
+  gulp.run(['html', 'pdf'])
+});
+
+gulp.task('default', function() {
+  gulp.run(['webserver'])
+  gulp.watch(source.md, ['html']);
+  gulp.watch(source.md, ['pdf']);
+});
